a1cbaa09e36a0635cc3f9fa79bcb515c35459e10
Kürşat Kaya <30809805+kursatfelsen@users.noreply.github.com>
2019-11-13 18:32:38 +0300

    Update BacMutationFrec.py

--- BacMutationFrec.py
+++ BacMutationFrec.py
@@ -1,7 +1,6 @@
-
-
 import random
 #fonksiyonlar
+#BAKTERİ SAYISINA GÖRE ÖLÜM KALIM ORANINI DEĞİŞTİR
 def LiveAndDeath(x):
     olumkalim = random.randint(1,100)
     if olumkalim<50:
@@ -9,14 +8,16 @@ def LiveAndDeath(x):
 
 def Produce(x):
     urun= random.randint(1,100)
+    Vara=0
+    Varb=0
     if urun>= 75:
-        if (isinstance(x.find("x"),int)):
+        if (x.find("x")!= -1):
             Vara = len(LivingBacterias)
             Varb = "xBac{}".format(Vara)
             LivingBacterias.append(Varb)
         else:
             Varb = "Bac{}".format(Vara)
-
+            LivingBacterias.append(Varb)
 #ESAS KISIM
 x = int(input("Kaç Adet Bakteriyle Başlamak istiyorsunuz:"))
 
@@ -40,7 +41,7 @@ while (i2<y):
 #Döngüye Başlayalım artık
 
 z = int(input("Kaç Döngü Olsun:"))
-
+print(LivingBacterias)
 i3 = 0 #Döngü Sayacı
 
 while (i3 < z):
@@ -52,26 +53,15 @@ while (i3 < z):
 
 
 
-print(LivingBacterias)
+Mutasyonlubakterisayisi = 0
+Mutasyonsuzbakterisayisi = 0
 
+for i in LivingBacterias:
+    if (i.find("x") == 0):
+        Mutasyonlubakterisayisi+=1
+    else:
+        Mutasyonsuzbakterisayisi+=1
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print("Başlangıçtaki Bakteri Sayısı:{}\nGeçen Cycle Sayısı:{}".format(x,z))
+print("{}. Cycle'dan Sonraki Bakteri Sayısı:{}".format(z,len(LivingBacterias)))
+print("Mutasyonlu Bakteri sayısı:{}\nMutasyonu Olmayan Bakteri Sayısı:{}".format(Mutasyonlubakterisayisi,Mutasyonsuzbakterisayisi))
